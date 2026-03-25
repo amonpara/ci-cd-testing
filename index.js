be414cbb3df395330840ecfe7be51a2a5f2560ff
amonpara <amonpara305@gmail.com>
2026-03-25 07:27:01 +0000

    change in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 CI/CD Pipeline Working Successfully!");
+  res.send("🚀 CI/CD Pipeline Working Successfully! I'll also check with new push in my Git repo");
 });
 
 app.listen(PORT, () => {
